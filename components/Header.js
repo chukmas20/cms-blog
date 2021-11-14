7575575b824481aa1f781f6b5278b8ae9475e3f4
chukmas20 <74096185+chukmas20@users.noreply.github.com>
2021-11-14 12:06:10 +0100

    slug

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,27 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Link from 'next/link';
 
-const categories = [
-    {
-    name:"React",
-    slug:"react"
-   },
-   {
-    name:"sport development",
-    slug:"sport-dev"
-   },
-]
+import { getCategories } from '../services';
+
 
 const Header = () => {
+
+    const [categories, setCategories] = useState([])
+
+      useEffect(()=>{
+          getCategories()
+          .then((newCategories)=> setCategories(newCategories))
+      },[])
+
+      
     return (
-        <div className="container mx-auto px-10 mbb-8">
+        <div className="container mx-auto px-10 mb-8">
             <div className="border-b w-full inline-block border-blue-400 py-8">
                 <div className="md:float-left block">
                     <Link href="/">
                         <span className="cursor-pointer font-bold text-4xl text-white">
-                             Graph CMS
+                             Personal Blog
                         </span>
                     </Link>
                 </div>
--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import {Header} from "./";
 
 const Layout = ({children}) => {
     return (
-        <>
+        <> 
            <Header />
            {children}
         </>
